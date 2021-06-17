88b2bfed3a7450fd58b4ae4806382f7d6153b37c
Nathan <nathan.flacher.pro@gmail.com>
2021-06-17 16:58:59 +0200

    404

---
+++ view/404.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--___Bootstrap style___-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <title>page not found</title>
+</head>
+
+<body class="vh-100 vw-100 text-danger d-flex flex-column justify-content-center align-items-center">
+    <h1>erreur <strong>404</strong></h1>
+    <p>page non trouvé</p>
+</body>
+
+</html>
